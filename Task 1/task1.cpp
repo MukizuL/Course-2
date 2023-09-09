f0ea0a11a32f280a21573d99636526c4893b31f9
MukizuL <53896777+MukizuL@users.noreply.github.com>
2023-09-09 08:47:34 +0700

    Task 1

---
+++ Task 1/task1.cpp
@@ -0,0 +1,34 @@
+﻿//variant 4
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+
+struct Name
+{
+	double x;
+	double y;
+	string color;
+};
+
+
+int main()
+{
+	ifstream in("in.txt");
+	vector<Name> names;
+	size_t n = 0;
+
+	cout << "X" << "          " << "Y" << "          " << "Color" << endl;
+
+	while (in.eof() != true)
+	{
+		Name output;
+		in >> output.x >> output.y >> output.color;
+		names.push_back(output);
+
+		cout << names[n].x << "         " << names[n].y << "          " << names[n].color << endl;
+		n++;
+	}
+}
